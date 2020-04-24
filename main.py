e153c02e69195a7f72d8bf9e4c44966ab4feef19
Clara Berard <claraberard@MacBook-Pro-de-lajavaness.local>
2020-04-24 13:39:49 +0200

    Transform path parameters to Path FastAPI

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from typing import List
-from question import read_question, add_question
+from question import read_question, add_question, get_maximum_question_id
 from answer import read_answer, check_answer
 from pydantic import BaseModel
 from prerequis import read_json, write_json
-#from add_question import add_question(question, questions_list)
+
 
 
 path_list_questions = "./questions.json"
@@ -13,8 +13,10 @@ questions_list = read_json(path_list_questions)
 
 app = FastAPI()
 
+max_question_id = get_maximum_question_id(questions_list)
+
 class QuestionStep(BaseModel): 
-    step: int
+    step: int = 1
     indice: str
 
 class Question(BaseModel):
@@ -22,34 +24,37 @@ class Question(BaseModel):
     answer: str
         
 class Answer(BaseModel):
-    answer: str
+    answer: str = ""
 
-        
+
+# Accueil
 @app.get("/")
 def get_root():
     return {"Hello": "Bienvenue à Question Pour Un Champion"}
 
 
-@app.post("/questions/")
+## Ajouter question
+@app.post("/question/propose")
 def create_item(question: Question):
     updated_question_list = add_question(question.question_content, question.answer, questions_list)
     write_json(updated_question_list['questions'], path_list_questions)
     return updated_question_list["result"]
 
 
-@app.get("/question/{question_id}")
-def get_question(question_id: int):
+## Lire question
+@app.get("/question/read/{question_id}")
+def get_question(question_id: int=Path(..., le=max_question_id)):
+    print('yolo')
     return {"question": read_question(question_id, questions_list)}
 
-
-
-@app.get("/question/{question_id}/solution")
-def get_answer(question_id: int):
+## Réponse question
+@app.get("/question/read/solution/{question_id}")
+def get_answer(question_id: int=Path(..., le=max_question_id)):
     return {"answer": read_answer(question_id, questions_list)}
 
-
-@app.post("/question/{question_id}/proposition")
-def get_answer(question_id:int, answer: Answer):
+## Répondre à une question
+@app.post("/question/repondre/{question_id}")
+def get_answer(*, question_id:int=Path(..., le=max_question_id), answer: Answer):
     return check_answer(answer.answer, question_id, questions_list)
     
 
--- question.py
+++ question.py
@@ -1,5 +1,7 @@
 from prerequis import find_items_in_list_dict, write_json
-    
+
+def transform_question_to_readable_dict(question): 
+    return [q.dict() for q in question]
 
 def read_question(question_id, list_questions): 
     
@@ -12,12 +14,11 @@ def read_question(question_id, list_questions):
     return question['question_content']
 
 
-def before_add_find_id(questions_list): 
+def get_maximum_question_id(questions_list): 
     """
     Find an unused id for question
     """
-    return max([question['question_id'] for question in questions_list])+1
-
+    return max([question['question_id'] for question in questions_list])
 
 
 def before_add_sanity_check(question, answer):
@@ -27,11 +28,11 @@ def before_add_sanity_check(question, answer):
 
 
 def add_question(question, answer, questions_list): 
-    question_id = before_add_find_id(questions_list)
+    question_id = get_maximum_question_id(questions_list)+1
     
     #question, answer  = before_add_sanity_check(question, answer)
     
-    question_ready = {"question_id": question_id, "question_content": [q.dict() for q in question], "answer":answer}
+    question_ready = {"question_id": question_id, "question_content": transform_question_to_readable_dict(question), "answer":answer}
     questions_list.append(question_ready)
     print(questions_list)
 
