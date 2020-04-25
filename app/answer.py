7f0d9bc96347ae1bdcc096c476414f96e8367914
Clara Berard <claraberard@MacBook-Pro-de-lajavaness.local>
2020-04-25 18:32:03 +0200

    adding Levenstein distance to accept answer

--- app/answer.py
+++ app/answer.py
@@ -1,5 +1,6 @@
 from app.prerequis import find_items_in_list_dict
-
+from app.transform_string import transform_text
+from app.check_answer import check_answer_correct
     
 
 def read_answer(question_id, list_questions): 
@@ -22,8 +23,10 @@ def check_answer(guessed_answer, question_id, list_questions):
     if not accepted_answers["status"]: 
         return {'guessed_answer': guessed_answer, "status":False, 'description':"QUESTION NOT FOUND"}
     answers_list = accepted_answers["answer_correct"]
+    guessed_answer_cleaned = transform_text(guessed_answer.answer_content)
     for answer in answers_list: 
-        if answer["answer_content"] == guessed_answer.answer_content: 
+        answer_cleaned = transform_text(answer['answer_content'])
+        if check_answer_correct(guessed_answer_cleaned, answer_cleaned): 
             return {'guessed_answer': guessed_answer, "status":True, 'eval_answer':True}
     return {'guessed_answer': guessed_answer, "status":True, 'eval_answer':False}
     
