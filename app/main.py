efa2367112053f4979dee37cd66a84da4b353a5a
Clara Berard <claraberard@MacBook-Pro-de-lajavaness.local>
2020-04-27 19:03:52 +0200

    add Response Model to get Question

--- app/main.py
+++ app/main.py
@@ -46,6 +46,7 @@ class GuessedAnswer(BaseModel):
     text: str = Field(..., example="Le Père Noël", max_length=100, title="Text guessed for the answer")
     duration: str = Field("00:01:00", example="hh:mm:ss", length =8, title="Duration to answer question")
 
+
 class Question(BaseModel):
     question_content: List[QuestionStep] = Field(
         ..., 
@@ -55,6 +56,18 @@ class Question(BaseModel):
         title="List of indices guess the correct answer")
     accepted_answers : List[Answer] = Field(..., min_items=1, example=[Answer(answer_content="Le père Noël", is_principal=True), Answer(answer_content="Père Noël", is_principal=False)], title="The correct possible Answers")
 
+
+class ReadQuestion(BaseModel): 
+    question: List[QuestionStep] = Field(
+        ..., 
+        example=[
+            QuestionStep(step=1, indice="J'apporte des cadeaux sous le sapin"), 
+            QuestionStep(step=2, indice="J'entre dans les maisons par la cheminée")])
+            
+    question_length : int = Field(..., example=2)
+    step_forward : bool = False
+
+
 # Accueil
 @app.get("/", response_model=Dict[str,str])
 def get_root():
@@ -62,6 +75,8 @@ def get_root():
 
 
 ## Ajouter question
+
+
 @app.post("/question/add", response_model=Dict[str, str])
 def create_item(question: Question):
     sanity_check = before_add_sanity_check(question)
@@ -74,7 +89,11 @@ def create_item(question: Question):
 
 
 ## Lire question
-@app.get("/question/read/{question_id}")
+
+
+      
+
+@app.get("/question/read/{question_id}", response_model=ReadQuestion)
 def get_question(question_id: int=Path(..., ge=1, le=max_question_id), step: int=Query(None, ge=1)):
     
     """
@@ -91,7 +110,7 @@ def get_question(question_id: int=Path(..., ge=1, le=max_question_id), step: int
         return {'question': step_found["question_content"], 'step_forward': step_found["next_step"], "question_length": step_found["question_lengthto q"]}
 
     question_found = read_question(question_id, questions_list)
-    return {'question':question_found['question_content'], "question_length": question_found["question_length"],'step_forward': False}
+    return {'question':question_found['question_content'], "question_length": question_found["question_length"]}
 
 
 ## Réponse question
