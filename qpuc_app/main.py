04be65557636b388c492aa1dc532d85748abdc5d
Clara Berard <claraberard@MacBook-Pro-de-lajavaness.local>
2020-05-09 00:17:04 +0200

    Modify cors

--- qpuc_app/main.py
+++ qpuc_app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["PUT", "POST", "GET", "DELETE"],
     allow_headers=["*"],
 )
 
